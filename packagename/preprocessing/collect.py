0317e71e401b237d98cff48ffe69f6fb69de7513
Carlos Pinto <cfpintog@gmail.com>
2021-12-01 15:29:47 -0500

    modificacion modulo collect, columnas adicionales

--- packagename/preprocessing/collect.py
+++ packagename/preprocessing/collect.py
@@ -8,7 +8,6 @@ from os import path, getcwd
 import pandas as pd
 import io 
 
-
 __URL="https://www.datos.gov.co/resource/xvdy-vvsk.json"
 __headers = {
         'Accept':'application/json'
@@ -22,7 +21,7 @@ def getLocalDataFrame(src: str)->pd.DataFrame:
     """
     Get a pandas dataframe from JSON file
     =====================================
-    Args
+    Params
         src: str
             Source path file
     Returns
@@ -32,65 +31,83 @@ def getLocalDataFrame(src: str)->pd.DataFrame:
     file = path.join(getcwd, src)
     return pd.read_json(file)
 
-def getDataFrameDataByYear( year:int, total_data= 200_000)->pd.DataFrame:
+def getDataFrameDataByYear( year:int, *extra_columns:str, total_data= 200_000)->pd.DataFrame:
     """
     Get a pandas dataframe from Secop API
     =====================================
-    Args
-            year: Int
-                    year that will get registers
-            total_data: Int, optional
-                    max number of items that will collect from api
+    Params
+        year: Int
+                year that will get registers
+        *extra_columns: str 
+                extra columns to extract data
+        total_data: Int, optional
+                max number of items that will collect from api
     returns
-            secopDataFrame: Pandas.DataFrame
-                    dataframe that contains the data of the specified year
+        secopDataFrame: Pandas.DataFrame
+                dataframe that contains the data of the specified year
     """
-    secopData = __requestDataByYear( year, total_data)
+    secopData = __requestDataByYear( year, total_data, *extra_columns)
     secopData = secopData.content
     secopDataFrame = pd.read_json(io.StringIO(secopData.decode('utf-8')))
     return secopDataFrame
 
 
-def saveSecopJSON(year:int , total_data=200_000, dest='./', filename="datos_secop_")->None:
-    """ 
-    save a JSON file that contains secop data
-    =========================================
-    Args
-            year: Int
-                    year that will get registers
-            total_data: Int, optional
-                    max number of items that will collect from api
-            dest: str, optional
-                    file destination path
-            filename: str, optional
-                    output filename
-    """        
-    file = f'{filename}{year}.json'
-    file_path = path.join(getcwd(), dest, file)
-    secopData = __requestDataByYear(year, total_data).json()
+def saveSecopJSON(year:int , *extra_columns:str, total_data=200_000, dest='./', filename="datos_secop_")->None:
+        """ 
+        save a JSON file that contains secop data
+        =========================================
+        Params
+        year: Int
+                year that will get registers
+        *extra_columns: str 
+                extra columns to extract data
+        total_data: Int, optional
+                max number of items that will collect from api
+        dest: str, optional
+                file destination path
+        filename: str, optional
+                output filename
+        """        
+        file = f'{filename}{year}.json'
+        file_path = path.join(getcwd(), dest, file)
+        secopData = __requestDataByYear(year, total_data, *extra_columns).json()
 
-    with open( file_path ,'w') as f:
-            json.dump(secopData, f)
-    
-    print(f"Total data writed in {file} {len(secopData)}")
+        with open( file_path ,'w') as f:
+                json.dump(secopData, f)
+
+        print(f"Total data writed in {file} {len(secopData)}")
 
 
-def __requestDataByYear( year:int, total_data:int)->requests.Response:
-    """
-    generate a request to Secop API
-    =============================
-    year: Int
-            year of the data
-    total_data: Int
-            max number of items that will get from the api
-    """
-    __params["anno_firma_del_contrato"] = year
-    __params["$limit"] = str(total_data)
-    secopData = requests.get(__URL,
-            headers=__headers, 
-            params=__params)
-    secopData.raise_for_status()
-    return secopData
+def __requestDataByYear( year:int, total_data:int, *extra_columns:str)->requests.Response:
+        """
+        generate a request to Secop API
+        =============================
+        Params
+        year: Int
+                year of the data
+        total_data: Int
+                max number of items that will get from the api
+        *extra_columns: str 
+                extra columns to extract data
+        """
+        params = dict(__params)
+        params['anno_firma_del_contrato'] = year
+        params['$limit'] = str(total_data)
+        
+        if len(extra_columns):
+                params['$select'] = params['$select']+","+",".join(extra_columns)
+
+        secopData = requests.get(__URL,
+                headers=__headers, 
+                params=params)
+        try:
+                secopData.raise_for_status()
+                
+        except requests.exceptions.HTTPError:
+                print("Error en la peticion revise las columnas adicionales")
+                return None
+        else: 
+                return secopData
     
 
 
