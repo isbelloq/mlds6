bd113e7e240d13153661ee9e5157f033d6ba32f7
Santiago Bello <isbelloq@unal.edu.co>
2021-12-05 18:58:58 -0500

    Cambio de directorio en main.py

--- scripts/data_acquisition/main.py
+++ scripts/data_acquisition/main.py
@@ -4,10 +4,10 @@ import os
 
 if __name__=="__main__":
     BASE_DIRECTORY = os.environ['PROJECT_DIRECTORY']
-    saveSecopJSON(2018, dest=os.path.join(BASE_DIRECTORY,'data'))
-    saveSecopJSON(2017, dest=os.path.join(BASE_DIRECTORY,'data'))
-    saveSecopJSON(2016, dest=os.path.join(BASE_DIRECTORY,'data'))
-    saveSecopJSON(2019, dest=os.path.join(BASE_DIRECTORY,'data'))
+    saveSecopJSON(2018, dest=os.path.join(BASE_DIRECTORY,'data/raw'))
+    saveSecopJSON(2017, dest=os.path.join(BASE_DIRECTORY,'data/raw'))
+    saveSecopJSON(2016, dest=os.path.join(BASE_DIRECTORY,'data/raw'))
+    saveSecopJSON(2019, dest=os.path.join(BASE_DIRECTORY,'data/raw'))
 
     
 
