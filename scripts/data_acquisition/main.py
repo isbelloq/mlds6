35f1d9daea55d91a462d757ea08a0142af19a46d
KendyLuz <kendyluz@gmail.com>
2021-12-05 17:29:01 -0500

    Mi primer commit

--- scripts/data_acquisition/main.py
+++ scripts/data_acquisition/main.py
@@ -3,11 +3,9 @@ from collect import saveSecopJSON
 import os
 
 if __name__=="__main__":
-    BASE_DIRECTORY = os.environ['PROJECT_DIRECTORY']
-    saveSecopJSON(2018, dest=os.path.join(BASE_DIRECTORY,'data'))
-    saveSecopJSON(2017, dest=os.path.join(BASE_DIRECTORY,'data'))
-    saveSecopJSON(2016, dest=os.path.join(BASE_DIRECTORY,'data'))
-    saveSecopJSON(2019, dest=os.path.join(BASE_DIRECTORY,'data'))
+
+    saveSecopJSON(2018, dest='./')
+    
 
     
 
